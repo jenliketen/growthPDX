5303a69683e2b061c2216bceda093b1aeae81087
Jenny Wang <wang.jennifer.1996@gmail.com>
2018-12-10 10:22:23 -0500

    Typo fixes

--- growth/source/diff_gene_exp_analysis.R
+++ growth/source/diff_gene_exp_analysis.R
@@ -20,9 +20,9 @@ growthClass$surClass <- ifelse(growthClass$Survival<median(growthClass$Survival)
 growthClass$slClass <- ifelse(growthClass$Slope<median(growthClass$Slope), "Low", "High")
 
 ## Visualize the classes
-growthClass.boxplot <- function(growthClass, varClass,varFeature) {
+growthClass.boxplot <- function(growthClassData, varClass,varFeature) {
   colClass <- c("#BC3C29F2", "#0072B5F2")
-  plot <- ggplot(data=growthClass, aes_string(x=varClass, y=varFeature, fill=varClass))
+  plot <- ggplot(data=growthClassData, aes_string(x=varClass, y=varFeature, fill=varClass))
   plot <- plot + geom_boxplot() + scale_fill_manual(values=colClass)
   plot <- plot + theme_bw() + theme(panel.border=element_blank(),
                               panel.grid.major=element_blank(),
@@ -36,9 +36,9 @@ growthClass.boxplot <- function(growthClass, varClass,varFeature) {
 
 pdf("growth_plot_classes.pdf",
     width=12, height=8)
-growthClass.boxplot(growthClass, "dtClass", "DoublingTime")
-growthClass.boxplot(growthClass, "surClass", "Survival")
-growthClass.boxplot(growthClass, "slClass", "Slope")
+growthClass.boxplot(growthClassData=growthClass, varClass="dtClass", varFeature="DoublingTime")
+growthClass.boxplot(growthClassData=growthClass, varClass="surClass", varFeature="Survival")
+growthClass.boxplot(growthClassData=growthClass, varClass="slClass", varFeature="Slope")
 dev.off()
 
 
@@ -66,34 +66,34 @@ table(eSet.subsetByN$tumor.type, extremes$dtClass) ## Make sure all tissue types
 
 pdf("dtClasses_plot.pdf",
     width=12, height=8)
-dtClasses_plot <- growthClass.boxplot(growthClass=extremes, varClass="dtClass", varFeature="DoublingTime")
+growthClass.boxplot(growthClassData=extremes, varClass="dtClass", varFeature="DoublingTime")
 dev.off()
 
 
 # Preprocessing for the expression matrix; generate Z-scores
 ## The function assumes that the column means are zero
-CenterANDScal <- function(trData, tsData=NULL)
-{
-  preProcValues <- caret::preProcess(trData, method = c("center", "scale")) 
+centerAndScale <- function(trData, tsData=NULL) {
+  preProcValues <- caret::preProcess(trData, method=c("center", "scale")) 
   trS <- predict(preProcValues, trData)
   
-  if(is.null(tsData)){ return(trS) }
+  if (is.null(tsData)) {
+    return(trS)
+    }
   
   tsS <- predict(preProcValues, tsData) ## The tsData will be the TCGA data we feed in for ML later
-  return(list( tr_data= trS, ts_data= tsS, scale_fact = preProcValues ))
+  return(list(tr_data=trS, ts_data=tsS, scale_fact=preProcValues))
 }
 
 expS <- exprs(pdxun)[, extremes$id]
 expS <- expS[-which(apply(expS, 1, var)==0), ] ## Remove genes with zero variance or function complains
-expS <- t(CenterANDScal(t(expS))) ## Tranpose twice because we want our row means to be zero
+expS <- t(centerAndScale(t(expS))) ## Tranpose twice because we want our row means to be zero
 dim(expS)
 
 
 # Limma baseline model for t-test
 ## Ideally we would use DESeq2 or edgeR to correct for heteroscedasticity, but alas, we do not
 ## have raw count data
-doDiffExp <- function(mat, flab)
-{
+doDiffExp <- function(mat, flab) {
   design <- model.matrix(~flab)
   fitTrtMean <- limma::lmFit(mat, design)
   efit <- limma::eBayes(fitTrtMean)
